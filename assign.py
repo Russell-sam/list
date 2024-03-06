201b3babc395777c8f0b99c532393e5508215c1a
wangila russell <wangilarussell17@gmail.com>
2024-03-06 20:23:21 +0300

    first commit

---
+++ assign.py
@@ -0,0 +1,19 @@
+my_list = []
+
+# Append elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert an element at the second position
+my_list.insert(1, 15)
+
+# Extend the list with another list
+my_list.extend([50, 60, 70])
+
+# Remove the last element
+my_list.pop()
+my_list.sort()
+index = my_list.index(30)
+print(index)
---
+++ lesson.py
@@ -0,0 +1,5 @@
+numbers=[1,48,92,34,27]
+print(numbers[3])
+#append adds to list
+numbers.append(32)
+print("After append:",numbers)
